ea5787d56bed45b4640ff8649f4f773c81b7ba03
dblazeski96 <dblazeski96@gmail.com>
2020-04-12 04:07:49 +0200

    Added comments for firebase config template

--- src/services/firebase/firebaseDb.ts
+++ src/services/firebase/firebaseDb.ts
@@ -1,18 +1,23 @@
 import * as firebase from 'firebase'
 import initializeDb from './initializeDb'
 
+// This is a firebase config template.
+// You need to create your own firebase project and firebase realtime database,
+// after everything is ready you need to adjust this template accordingly.
 const firebaseConfig = {
-  apiKey: "AIzaSyDge3n9WM_smJb0BKxUBpLVgUZsX-AkGSs",
-  authDomain: "dex-portfolio.firebaseapp.com",
-  databaseURL: "https://dex-portfolio.firebaseio.com",
-  projectId: "dex-portfolio",
-  storageBucket: "dex-portfolio.appspot.com",
-  messagingSenderId: "214225539606",
-  appId: "1:214225539606:web:7172c3900ff52d36325b37",
-  measurementId: "G-YFD1DJNJFT"
-};
+  // apiKey: "api-key",
+  // authDomain: "project-id.firebaseapp.com",
+  // databaseURL: "https://project-id.firebaseio.com",
+  // projectId: "project-id",
+  // storageBucket: "project-id.appspot.com",
+  // messagingSenderId: "sender-id",
+  // appId: "app-id",
+  // measurementId: "G-measurement-id"
+}
 
 firebase.initializeApp(firebaseConfig)
+
+// In order anonymous authorization to work, you need to enable that also in your firebase project
 firebase.auth().signInAnonymously()
 
 const dbRef = firebase.database().ref()
